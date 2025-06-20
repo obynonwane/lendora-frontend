39915dcf8e65465398be5d4a5fbaedb8fc3d8b4d
Chibuike Nnaji <67190735+chinnaji@users.noreply.github.com>
2025-06-20 08:52:20 +0100

    fixed popup chat
    users can now close popup chat modal asking them to login (mobile)

--- app/components/PopupChatModal.tsx
+++ app/components/PopupChatModal.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
 import { useUserStore } from "@/app/utils/useUserStore";
 import { useRouter, usePathname } from "next/navigation";
-
+import { IoMdClose } from "react-icons/io";
 import ChatPage from "./ChatPage";
 function PopupChatModal() {
   const router = useRouter();
@@ -24,18 +24,20 @@ function PopupChatModal() {
         {!authStateLoaded ? (
           <span className=" rounded bg-zinc-100 animate-pulse h-full w-full"></span>
         ) : !isAuthenticated ? (
-          <div className="flex flex-col items-center pt-10  lg:order-3">
-            <h3 className="  px-5 pt-4 text-center font-medium text-slate-900 ">
-              Login to Chat
+          <div className="flex flex-col items-center pt-10 relative lg:order-3">
+            <h3 className="  px-5 pt-4 text-center font-medium flex items-center text-slate-900 ">
+              Login to Chat{" "}
             </h3>
-
+            <IoMdClose
+              onClick={() => setIsShowChatModal(false)}
+              className="text-lg absolute right-4 top-4 cursor-pointer"
+            />{" "}
             {/* <RiCloseLine
                         onClick={() => {
                           setIsShowPurchaseModal(false);
                         }}
                         className="text-xl cursor-pointer absolute top-2 right-2"
                       /> */}
-
             <div className="px-5 pb-5">
               <p className="text-center text-sm text-gray-700">
                 Please log in to chat.
@@ -65,7 +67,7 @@ function PopupChatModal() {
 
       <div
         onClick={() => setIsShowChatModal(!isShowChatModal)}
-        className="size-16 fixed right-10 bottom-7 cursor-pointer bg-orange-400 flex items-center justify-center rounded-full z-[99999999999999999999999999999999] "
+        className="size-16 fixed right-6 lg:right-10 bottom-7 cursor-pointer bg-orange-400 flex items-center justify-center rounded-full z-[99999999999999999999999999999999] "
       >
         <IoChatbubbleEllipsesSharp className="text-2xl text-white" />
       </div>
