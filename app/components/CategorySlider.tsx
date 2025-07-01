5c30855be2b10dad38b9bfd7073b6944bacdcdfc
Chibuike Nnaji <67190735+chinnaji@users.noreply.github.com>
2025-07-01 08:18:10 +0100

    Update CategorySlider.tsx

--- app/components/CategorySlider.tsx
+++ app/components/CategorySlider.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+
 import "swiper/css";
 import "swiper/css/autoplay";
 
